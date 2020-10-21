4d7ba40e7fd58de1ca66c649e1ff7ddec4d6cd99
MilesTEG1 <MilesTEG1@gmail.com>
2020-10-21 15:30:19 +0200

    Version fonctionnelle

--- hash_verif.sh
+++ hash_verif.sh
@@ -3,32 +3,13 @@
 ## README
 # /!\ Ce script d'installation est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
 # Utiliser la commande suivante pour rendre le script exécutable :
-# chmod 755 ./HomeBrew-Restore-Installation.sh
-
-## NOTE IMPORTANTE
-# Il faudra probablement modifier le script pour l'adapter à vos besoin. !
-# Ne l'excécuter pas sans l'avoir lu entièrement afin de vérifier que ce qui est sauvegarder/restaurer
-# correspond bien à vos attentes.
-#
-# Le script sauvegarde ceci :
-#       - la liste de tout ce qui a été installé avec HomeBrew (donc soit avec `brew install`, soit avec
-#         `brew cask install` et aussi les `brew tap`).
-#       - Certains paramètres par défaut (exemple : `defaults write com.apple.dock tilesize -int 32`)
-#       - Sauvegarde de certains fichiers de configuration : Oh My Zsh, et certains fichiers/dossiers
-#         présents dans ~/Library. Il faudra probablement modifier le script pour l'adapter à vos besoin.
-# Lors de la sauvegarde, le script va tester l'existance du dossier de destination ./Fichier :
-# Si ce dernier existe, il sera proposé de le supprimer ou de le renommer car le script doit commencer
-# avec un dossier vierge.
-# Tout ce qui sera sauvegarder (par copie directe ou par archivage) sera contenu dans ce fichier.
-# Veillez à ne pas toucher ce dossier pendant l'éxécution du script.
-#
+# chmod 755 ./hash_verif.sh
 
 ## SYNTAX
 #
-# syntax:   Backup-Restore-Apps-Script.sh [<ARG>]
-#           [<ARG>] :   RESTORE
-#                       BACKUP
-#                       -h ou h ou -help ou help ou --h ou --help
+# syntax:   Bhash_verif.sh [<ARG>]
+#           [<ARG>] :   fichier
+#                       -h ou -help ou --h ou --help
 
 ## LICENCE
 #
@@ -50,6 +31,7 @@ declare -r FALSE=1 # Read-only variable, i.e., a constant.
 
 declare -r nb_param=$#                           # Nombre d'argument(s) fourni(s) au script.
 declare -r param_1="$1"                          # 1er argument fourni
+declare -r param_2="$2"                          # 1er argument fourni
 declare -r nom_script=$0                         # Nom du script
 declare -r script_dir=$(cd ${0%/*} && pwd -P)    # Chemin complet d'accès du script
 declare -r script_dir_rel=$(dirname $0)          # Chemin d'accès relatif du script
@@ -111,17 +93,23 @@ f_affiche_parametre() {
     # syntax:   f_affiche_parametre [<argument1> [<argument2>]]
     #           Les arguments sont facultatifs...
     #
-    if [ -z "$1" ]; then
+
+    if [ -z "$1" ]; then    # $1 est vide
         echo "${WHITE}${RED_BG}Aucun paramètre n'a été fourni. Il faut fournir le fichier à hasher.${NORMAL}"
-    elif [ -n "$2" ]; then
+    elif [ -n "$1" ] && [ "$1" != "help" ]; then  # $1 n'est pas vide et n'est pas help
         echo "${YELLOW}Le paramètre fourni ${WHITE}${RED_BG} $1 ${NORMAL}${YELLOW} n'est pas correct. ${NORMAL}"
+    elif [ $# -ne 2 ]; then
+        echo "${YELLOW}Le nombre de paramètre fourni n'est pas correct : ${WHITE}${RED_BG} $* ${NORMAL}"
     else
-        echo "${YELLOW}Le nombre de paramètre fourni n'est pas correct : ${WHITE}${RED_BG} $1 ${NORMAL}"
+        echo "${YELLOW}Le paramètre fourni ${WHITE}${RED_BG} $1 ${NORMAL}${YELLOW} n'est pas correct. ${NORMAL}"
     fi
     echo
-    echo "${UNDERLINE}${WHITE}Utilisation du script :${NORMAL}\t${POWDER_BLUE}      $nom_script ${GREEN}[paramètre]${NORMAL}"
+    echo "${UNDERLINE}${WHITE}Utilisation du script :${NORMAL}\t${POWDER_BLUE}      $nom_script ${GREEN}type_hash fichier${NORMAL}"
     echo
-    echo "${UNDERLINE}${WHITE}Liste des [paramètre] utilisables :${GREEN} fichier${NORMAL}"
+    echo "${POWDER_BLUE}Le type de hash doit être une de ces options :    ${GREEN}sha256${NORMAL}"
+    echo "${POWDER_BLUE}                                                  ${GREEN}sha1${NORMAL}"
+    echo "${POWDER_BLUE}                                                  ${GREEN}md5${NORMAL}"
+    echo
 }
 
 
@@ -130,46 +118,63 @@ f_affiche_parametre() {
 
 
 clear # On efface l'écran
-echo "${WHITE}Ce script permet de vérifier le HASH md5/sha1/sha256 d'un fichier "
-echo "en collant le hash de comparaison.${NORMAL}"
+echo "${WHITE}Ce script permet de vérifier le HASH md5/sha1/sha256 d'un fichier sur un NAS Synology via SSH."
+echo "Il faudra coller le hash d'origine lorsqu'il sera demandé.${NORMAL}"
 echo
 
 if [ $nb_param -eq 0 ]; then
     # Aucun paramètre n'a été fourni. On va afficher la liste de ce qui peut être utilisé.
     f_affiche_parametre # On appelle la fonction qui affiche l'utilisation des paramètres
     exit
-elif [ $nb_param -ne 1 ]; then
-    # Il ne faut qu'un seul paramètre, donc s'il y a un nombre de paramètre différent de 1...
+elif [ $param_1 = "-h" ] || [ $param_1 = "-help" ] || [ $param_1 = "--h" ] || [ $param_1 = "--help" ] || [ $param_1 = "h" ] || [ $param_1 = "help" ]; then
+        # La paramètre entré est une demande d'affichage de la syntaxe...
+        f_affiche_parametre "help" # On appelle la fonction qui affiche l'utilisation des paramètres
+        exit
+elif [ $nb_param -ne 2 ]; then
+    # Il ne faut que deux paramètres, donc s'il y a un nombre de paramètre différent de 1...
     f_affiche_parametre "$*"
     exit
 else
+    
+    
     # On récupère le hash de comparaison :
-    echo "Entrez le hash de comparaison : "
+    echo "Entrez le hash de comparaison (faire un copier/coller): "
     read hash_origin
     echo
-    echo "Le hash d'origine à vérifier est : $hash_origin"
-    echo "Vérification du hash du fichier :"
-    echo
-    
-    
+
+    if [ -z $hash_origin ]; then
+        # aucun hash coller
+        echo ""
+        echo "${WHITE}${RED_BG}Aucun hash n'a été fourni.${NORMAL}"
+        echo "Interruption du script."
+        exit 99 # On stoppe immédiatement l'exécution de la fonction !
+    fi
+
+    echo "hash fourni = $hash_origin"
+    echo "paramètre n°1 = $param_1"
     case "$param_1" in
-    #######################################################################################################
-    "sha256")      # 
+        [Ss][Hh][Aa]256)      # 
+            echo "Vérification du hash SHA256 :"
+            echo "$hash_origin $param_2" | sha256sum -c
+            ;;
+            
+        #######################################################################################################
+        [Ss][Hh][Aa]1)      # 
+            echo "Vérification du hash SHA1 :"
+            echo "$hash_origin $param_2" | sha1sum -c
+            ;;
 
-        ;;
-    
-    #######################################################################################################
-    "sha1")      # 
-        ;;
+        #######################################################################################################
+        [Mm][Dd]5)      # 
+            echo "Vérification du hash md5 :"
+            echo "$hash_origin $param_2" | md5sum -c
+            ;;
 
-    #######################################################################################################
-    "md5")      # 
-        ;;
-
-    *)
-        f_affiche_parametre "$param_1" "param_inc" # On appelle la fonction qui affiche l'utilisation des paramètres
-        exit
-        ;;
+        *)
+        echo "${WHITE}${RED_BG}Erreur inattendue.${NORMAL}"
+        echo "Interruption du script."
+        exit 99 # On stoppe immédiatement l'exécution de la fonction !            exit
+            ;;
     esac
 fi
 echo "-----------------------------------  Fin du Script  -----------------------------------"
